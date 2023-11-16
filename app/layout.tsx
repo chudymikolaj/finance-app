370aebf4eea2acf49c9535535e164c6fb4a00edd
chudymikolaj <chudymikolaj06@gmail.com>
2023-11-16 22:19:01 +0100

    style: change quotes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,36 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-import StyledComponentsRegistry from '@/lib/RegistryStyledComponents'
-import ThemeProviderContext from '@/lib/ThemeProviderContext'
-import Navbar from "@/components/Navbar"
+import StyledComponentsRegistry from "@/lib/RegistryStyledComponents";
+import ThemeProviderContext from "@/lib/ThemeProviderContext";
+import Navbar from "@/components/Navbar";
 
-import '@/styles/globals.styled'
-import { GlobalStyle } from '@/styles/globals.styled';
+import "@/styles/globals.styled";
+import { GlobalStyle } from "@/styles/globals.styled";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Finance app',
-  description: 'Made by Chudy Mikołaj',
-}
+	title: "Finance app",
+	description: "Made by Chudy Mikołaj",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <StyledComponentsRegistry>
-          <ThemeProviderContext>
-            <Navbar />
-            {children}
-            <GlobalStyle />
-          </ThemeProviderContext>
-        </StyledComponentsRegistry>
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<StyledComponentsRegistry>
+					<ThemeProviderContext>
+						<Navbar />
+						{children}
+						<GlobalStyle />
+					</ThemeProviderContext>
+				</StyledComponentsRegistry>
+			</body>
+		</html>
+	);
 }
