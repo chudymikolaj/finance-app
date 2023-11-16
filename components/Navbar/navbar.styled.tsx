63ae20b967b722ba81685ea0b4ee1453e4b1bf9e
chudymikolaj <chudymikolaj06@gmail.com>
2023-11-16 22:20:17 +0100

    style: add brackets for pros

--- components/Navbar/navbar.styled.tsx
+++ components/Navbar/navbar.styled.tsx
@@ -3,69 +3,69 @@ import Link from "next/link";
 import styled from "styled-components";
 
 export const Navbar = styled.div`
-  width: calc(100% - 30px);
-  margin: 30px 15px;
+	width: calc(100% - 30px);
+	margin: 30px 15px;
 
-  @media (min-width: 768px) {
-    width: calc(100% - 10vh);
-    margin: 30px 5vh;
-  }
+	@media (min-width: 768px) {
+		width: calc(100% - 10vh);
+		margin: 30px 5vh;
+	}
 
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
 `;
 
 export const NavbarLogotype = styled(Link)`
-  font-weight: ${WEIGHT.medium};
+	font-weight: ${WEIGHT.medium};
 `;
 
 export const NavbarMenu = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
+	display: flex;
+	flex-direction: row;
+	gap: 10px;
 `;
 
 export const NavbarMenuNotification = styled(Link)`
-  padding: 5px;
+	padding: 5px;
 `;
 
 export const NavbarDropdownMenu = styled.div`
-  position: relative;
+	position: relative;
 `;
 
 export const NavbarDropdownMenuButton = styled.button`
-  padding: 5px;
-  background-color: unset;
-  border: unset;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 6px;
-  color: ${props => props.theme.color};
-  cursor: pointer;
+	padding: 5px;
+	background-color: unset;
+	border: unset;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 6px;
+	color: ${(props) => props.theme.color};
+	cursor: pointer;
 `;
 
-export const NavbarMenuDropdown = styled.div<{ $show?: boolean; }>`
-  min-width: calc(200px);
-  width: calc(100vw);
-  padding: 18px 30px;
-  background-color: ${props => props.theme.colorElement};
-  border: 1px solid ${props => props.theme.colorBorder};
-  border-radius: ${BORDER_RADIUS};
-  display: ${(props) => (props.$show ? "block" : "none")};
-  position: absolute;
-  top: calc(100% + 5px);
-  right: 0;
+export const NavbarMenuDropdown = styled.div<{ $show?: boolean }>`
+	min-width: calc(200px);
+	width: calc(100vw);
+	padding: 18px 30px;
+	background-color: ${(props) => props.theme.colorElement};
+	border: 1px solid ${(props) => props.theme.colorBorder};
+	border-radius: ${BORDER_RADIUS};
+	display: ${(props) => (props.$show ? "block" : "none")};
+	position: absolute;
+	top: calc(100% + 5px);
+	right: 0;
 
-  @media (min-width: 1200px) {
-    width: auto;
-  }
+	@media (min-width: 1200px) {
+		width: auto;
+	}
 `;
 
 const groupOfLinks = `
@@ -80,17 +80,17 @@ const groupOfLinks = `
 `;
 
 export const NavbarMenuList = styled.ul`
-  ${groupOfLinks};
+	${groupOfLinks};
 `;
 
 export const NavbarMenuListLink = styled(Link)`
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
+	display: flex;
+	flex-direction: row;
+	gap: 10px;
 
-  &:hover {
-    opacity: .5;
-  }
+	&:hover {
+		opacity: 0.5;
+	}
 `;
 
 const groupOfLinksBorder = `
@@ -99,26 +99,26 @@ const groupOfLinksBorder = `
 `;
 
 export const NavbarMenuListActiveProfile = styled.span`
-  ${groupOfLinksBorder};
-  border-bottom: 1px solid ${props => props.theme.colorLink};
-  color: ${props => props.theme.colorLink};
-  cursor: default;
+	${groupOfLinksBorder};
+	border-bottom: 1px solid ${(props) => props.theme.colorLink};
+	color: ${(props) => props.theme.colorLink};
+	cursor: default;
 `;
 
 export const NavbarMenuListProfiles = styled.ul`
-  ${groupOfLinksBorder};
-  border-bottom: 1px solid ${props => props.theme.colorLink};
+	${groupOfLinksBorder};
+	border-bottom: 1px solid ${(props) => props.theme.colorLink};
 `;
 
 export const NavbarMenuListRest = styled.ul`
-  ${groupOfLinks};
+	${groupOfLinks};
 `;
 
 export const NavbarMenuThemeToggle = styled.button`
-  width: 100%;
-  padding: 6px 16px;
-  margin: 10px auto 0;
-  border: 1px solid ${props => props.theme.colorLink};
-  border-radius: ${BORDER_RADIUS};
-  color: ${props => props.theme.color};
+	width: 100%;
+	padding: 6px 16px;
+	margin: 10px auto 0;
+	border: 1px solid ${(props) => props.theme.colorLink};
+	border-radius: ${BORDER_RADIUS};
+	color: ${(props) => props.theme.color};
 `;
