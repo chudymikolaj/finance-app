8d3fb6c690154318c3197f5ecca66e706ddfe8d6
chudymikolaj <chudymikolaj06@gmail.com>
2023-11-16 22:47:43 +0100

    feat: create AddExpenseButton component

---
+++ components/Buttons/AddExpenseButton/index.tsx
@@ -0,0 +1,22 @@
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+	className?: string;
+	dispatch: () => void;
+};
+
+export default function AddExpenseButton({
+	className,
+	dispatch,
+	...props
+}: ButtonProps) {
+	return (
+		<button
+			className={className}
+			onClick={dispatch}
+			{...props}
+		>
+			Nie zapłacono
+		</button>
+	);
+}
+
+let nextId = Math.random() * 10;
