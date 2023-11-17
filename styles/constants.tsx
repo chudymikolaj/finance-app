f52761fd93696841cc9c933df6fd77f98ea4b13d
chudymikolaj <chudymikolaj06@gmail.com>
2023-11-17 23:28:55 +0100

    feat: add constatns

--- styles/constants.tsx
+++ styles/constants.tsx
@@ -1,51 +1,52 @@
 export const STYLE_DARK_MODE = {
-  backgroundColor: "#0f0e17",
-  colorElement: "#242629",
-  colorElement_5: "rgba(15, 14, 23, .55)",
-  colorBorder: "#010101",
-  colorButton: "#ff8906",
-  colorButtonFont: "#fffffe",
-  color: "#fffffe",
-  colorRed: "#FF6969",
-  colorIcon: "#fffffe",
-  colorLink: "#fffffe",
-  colorHeadline: "#fffffe",
-  colorSubHeadline: "#94a1b2",
+	backgroundColor: "#0f0e17",
+	colorElement: "#242629",
+	colorElement_5: "rgba(238, 238, 238, .1)",
+	colorBorder: "#010101",
+	colorButton: "#ff8906",
+	colorButtonFont: "rgb(255, 255, 254)",
+	color: "#fffffe",
+	colorRed: "#FF6969",
+	colorIcon: "#fffffe",
+	colorLink: "#fffffe",
+	colorHeadline: "#fffffe",
+	colorSubHeadline: "#94a1b2",
 };
 
 export const STYLE_LIGHT_MODE = {
-  backgroundColor: "#fffffe",
-  colorElement: "#e3f6f5",
-  colorElement_5: "rgba(255, 255, 254, .55)",
-  colorBorder: "#e1eeed",
-  colorButton: "#272343",
-  colorButtonFont: "#fffffe",
-  color: "#2d334a",
-  colorRed: "#FF6969",
-  colorIcon: "#2d334a",
-  colorLink: "#2d334a",
-  colorHeadline: "#272343",
-  colorSubHeadline: "#2d334a",
+	backgroundColor: "#fffffe",
+	colorElement: "#e3f6f5",
+	colorElement_5: "rgba(238, 238, 238, .55)",
+	colorBorder: "#e1eeed",
+	colorButton: "#272343",
+	colorButtonFont: "#fffffe",
+	color: "#2d334a",
+	colorRed: "#FF6969",
+	colorIcon: "#2d334a",
+	colorLink: "#2d334a",
+	colorHeadline: "#272343",
+	colorSubHeadline: "#2d334a",
 };
 
 export const SIZES = {
-  smallText: "14px",
-  normalText: "16px",
-  bigText: "19px",
-  subText: "24px",
-  headerText: "32px",
-}
+	smallText: "14px",
+	normalText: "16px",
+	bigText: "19px",
+	subText: "24px",
+	headerText: "32px",
+};
 export const WEIGHT = {
-  light: "300",
-  normal: "400",
-  medium: "600",
-  semiBold: "700",
-  bold: "800",
-}
-
+	light: "300",
+	normal: "400",
+	medium: "600",
+	semiBold: "700",
+	bold: "800",
+};
 
 export const CUBICS = {
-  easyOut: "cubic-bezier(0.25,0.46,0.45,0.94)",
-}
+	easyOut: "cubic-bezier(0.25,0.46,0.45,0.94)",
+};
 
 export const BORDER_RADIUS = "5px";
+export const BORDER_RADIUS_10 = "10px";
+export const FONT_SIZE_BUTTONS = "10px";
