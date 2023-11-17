ad0e4672bf7d642aaf3be0acac37c7e8dc96990b
chudymikolaj <chudymikolaj06@gmail.com>
2023-11-17 23:36:27 +0100

    feat: create CashFlow component

---
+++ components/CashFlow/CashFlow.styled.tsx
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+import Button from "@/components/Buttons/Button";
+
+import {
+	FlexRow,
+	FlexRowSpaceBetween,
+	FlexRowStart,
+	FlexRowAlignCenter,
+	FlexCenter,
+} from "@/styles/mixins.styled";
+
+import { CUBICS, FONT_SIZE_BUTTONS } from "@/styles/constants";
+
+export const FlowCashContainer = styled.div`
+	margin: auto;
+`;
+
+export const FlowCashHeader = styled.div`
+	${FlexRowSpaceBetween}
+
+	padding: 10px 0;
+	margin: 0 auto 10px;
+	border-bottom: 1px solid ${(props) => props.theme.colorElement_5};
+	gap: 15px;
+`;
+
+export const FlowCashHeaderTitle = styled.div`
+	font-size: 22px;
+	font-weight: 800;
+`;
+
+export const FlowCashHeaderOptions = styled.div`
+	${FlexRowAlignCenter}
+	gap: 15px;
+`;
+
+export const FlowCashHeaderMonth = styled.div`
+	font-size: 22px;
+	font-weight: 800;
+`;
+
+export const FlowCashHeaderButtons = styled.div`
+	${FlexRow}
+	gap: 5px;
+`;
+
+export const FlowCashTabs = styled.div`
+	${FlexRowStart}
+	gap: 15px;
+`;
+
+export const FlowCashTab = styled.div<{ $isActive: boolean }>`
+	${FlexRow}
+
+	opacity: ${(props) => (props.$isActive ? 0.4 : 1)};
+	transition: opacity ${CUBICS};
+	font-size: 12px;
+	cursor: pointer;
+`;
+
+export const CashFlowButton = styled(Button)`
+	font-size: ${FONT_SIZE_BUTTONS};
+
+	div {
+		height: 15px;
+		width: 15px;
+		${FlexCenter}
+	}
+
+	svg {
+		height: 15px;
+		width: 15px;
+	}
+`;
---
+++ components/CashFlow/index.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { Suspense, useEffect, useState } from "react";
+
+import CashFlowList from "./CashFlowList";
+
+import {
+	FlowCashContainer,
+	FlowCashHeader,
+	FlowCashHeaderTitle,
+	FlowCashHeaderOptions,
+	FlowCashHeaderMonth,
+	FlowCashHeaderButtons,
+	FlowCashTabs,
+	FlowCashTab,
+	CashFlowButton,
+} from "./CashFlow.styled";
+
+import { useAppContext } from "@/lib/ThemeProviderContext/actions";
+
+const CashFlowComponent = () => {
+	const [useTab, setuseTab] = useState("expenses");
+	const { expenses, revenues, updateExpenses } = useAppContext();
+	const isExpenses = useTab === "expenses";
+	const isRevenues = useTab === "revenues";
+
+	useEffect(() => {
+		const sumExpenses = expenses.reduce((val, currentValue) => {
+			return val + currentValue.value;
+		}, 0);
+
+		updateExpenses(sumExpenses);
+	}, [expenses]);
+
+	return (
+		<FlowCashContainer>
+			<FlowCashTabs>
+				<FlowCashTab
+					onClick={() => setuseTab("expenses")}
+					$isActive={useTab === "expenses"}
+				>
+					Wydatki
+				</FlowCashTab>
+				<FlowCashTab
+					onClick={() => setuseTab("revenues")}
+					$isActive={useTab === "revenues"}
+				>
+					Przychód
+				</FlowCashTab>
+			</FlowCashTabs>
+
+			<FlowCashHeader>
+				<FlowCashHeaderTitle>Wydatki na miesiąc</FlowCashHeaderTitle>
+				<FlowCashHeaderOptions>
+					<FlowCashHeaderMonth>09.2023</FlowCashHeaderMonth>
+					<FlowCashHeaderButtons>
+						<CashFlowButton svgUrl="/add.svg" />
+						<CashFlowButton svgUrl="/more_vert.svg" />
+					</FlowCashHeaderButtons>
+				</FlowCashHeaderOptions>
+			</FlowCashHeader>
+
+			<Suspense fallback={<Loader />}>
+				{isExpenses && (
+					<CashFlowList
+						type="expense"
+						items={expenses}
+					/>
+				)}
+				{isRevenues && (
+					<CashFlowList
+						type="revenues"
+						items={revenues}
+					/>
+				)}
+			</Suspense>
+		</FlowCashContainer>
+	);
+};
+
+function Loader() {
+	return <p>Ładowanie...</p>;
+}
+
+export default CashFlowComponent;
