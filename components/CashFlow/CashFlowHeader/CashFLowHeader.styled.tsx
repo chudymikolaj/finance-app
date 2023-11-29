6f4e2fe0ddd26f35a545a8ac22d35319355fbd81
chudymikolaj <chudymikolaj06@gmail.com>
2023-11-29 23:30:41 +0100

    refactore: add form and style of CashFlowHeader

--- components/CashFlow/CashFlowHeader/CashFLowHeader.styled.tsx
+++ components/CashFlow/CashFlowHeader/CashFLowHeader.styled.tsx
@@ -2,46 +2,114 @@ import styled from "styled-components";
 
 import Button from "@/components/Buttons/Button";
 
+import { BORDER_RADIUS, SIZES, CUBICS } from "@/styles/constants";
+
 import {
 	FlexRow,
 	FlexRowSpaceBetween,
 	FlexRowAlignCenter,
 	FlexCenter,
+	FlexColumn,
 } from "@/styles/mixins.styled";
 
 import { FONT_SIZE_BUTTONS } from "@/styles/constants";
 
-export const FlowCashContainer = styled.div`
+export const CashFlowContainer = styled.div`
 	margin: auto;
 `;
 
-export const FlowCashHeader = styled.div`
+export const CashFlowHeader = styled.div`
 	${FlexRowSpaceBetween}
 
 	padding: 10px 0;
 	margin: 0 auto 10px;
-	border-bottom: 1px solid ${(props) => props.theme.colorElement_5};
 	gap: 15px;
+	position: relative;
+
+	&:after {
+		content: "";
+		width: 100%;
+		height: 1px;
+		border-bottom: 1px solid ${(props) => props.theme.colorElement_5};
+		position: absolute;
+		bottom: 0;
+		left: 0;
+	}
 `;
 
-export const FlowCashHeaderTitle = styled.div`
+export const CashFlowHeaderTitle = styled.div`
 	font-size: 22px;
 	font-weight: 800;
 `;
 
-export const FlowCashHeaderOptions = styled.div`
+export const CashFlowHeaderOptions = styled.div`
 	${FlexRowAlignCenter}
 	gap: 10px;
 `;
 
-export const FlowCashHeaderMonth = styled.div`
+export const CashFlowHeaderMonth = styled.div`
 	font-size: 22px;
 	font-weight: 800;
 `;
 
-export const FlowCashHeaderButtons = styled.div`
+export const CashFlowHeaderButtons = styled.div`
 	${FlexRow}
 	gap: 5px;
+`;
+
+export const CashFlowSortByDate = styled.div<{ $animate?: boolean }>`
+	width: 100%;
+	margin: auto;
+	padding: 16px;
+	background-color: ${(props) => props.theme.colorElement};
+	border: 1px solid ${(props) => props.theme.colorBorder};
+	border-radius: ${BORDER_RADIUS};
+	visibility: ${(props) => (props.$animate ? "visible" : "hidden")};
+	opacity: ${(props) => (props.$animate ? "1" : "0")};
+	position: absolute;
+	top: 36px;
+	left: 0;
+	transform: ${(props) =>
+		props.$animate ? "translateY(5px)" : "translateY(0)"};
+	transition: transform ${CUBICS.easyOut} 200ms, opacity ${CUBICS.easyOut} 200ms,
+		visibility ${CUBICS.easyOut} 200ms;
+	z-index: 1;
+`;
+
+export const CashFlowSortByDateForm = styled.form`
+	${FlexColumn}
+	gap: 10px;
+`;
+
+export const CashFlowSortByDateLabel = styled.label`
+	display: block;
+`;
+export const CashFlowSortByDateInput = styled.input`
+	padding: 10px 16px;
+	background-color: ${(props) => props.theme.colorElement_5};
+	border: 0;
+	border-radius: ${BORDER_RADIUS};
+	font-family: inherit;
+	color: ${(props) => props.theme.color};
+
+	&::-webkit-calendar-picker-indicator {
+		filter: invert(1);
+		cursor: pointer;
+	}
+`;
+
+export const CashFlowSortByDataSort = styled.button`
+	width: 100%;
+	padding: 10px 16px;
+	background-color: ${(props) => props.theme.colorBackgroundActiveButton};
+	border-radius: ${BORDER_RADIUS};
+	font-size: ${SIZES.smallText};
+	color: ${(props) => props.theme.colorTextActiveButton};
+	transition: background-color ${CUBICS.easyOut} 200ms;
+
+	&:hover {
+		background-color: ${(props) => props.theme.colorElement_5};
+	}
 `;
 
 export const CashFlowButton = styled(Button)`
--- components/CashFlow/CashFlowHeader/index.tsx
+++ components/CashFlow/CashFlowHeader/index.tsx
@@ -1,23 +1,105 @@
+import { useState, type ChangeEvent, type FormEvent } from "react";
+
 import {
-	FlowCashHeader,
-	FlowCashHeaderTitle,
-	FlowCashHeaderOptions,
-	FlowCashHeaderMonth,
-	FlowCashHeaderButtons,
+	CashFlowHeader,
+	CashFlowHeaderTitle,
+	CashFlowHeaderOptions,
+	CashFlowHeaderMonth,
+	CashFlowHeaderButtons,
+	CashFlowSortByDate,
 	CashFlowButton,
-} from "./CashFLowHeader.styled";
+	CashFlowSortByDateForm,
+	CashFlowSortByDateLabel,
+	CashFlowSortByDateInput,
+	CashFlowSortByDataSort,
+} from "./CashFlowHeader.styled";
 
 const CashFlowHeaderComponent = () => {
+	const currentDate = new Date();
+	const FIRST_DAY_OF_MONTH = new Date(
+		currentDate.getFullYear(),
+		currentDate.getMonth(),
+		1
+	);
+
+	const GET_FIRST_DAY_MONTH = `${FIRST_DAY_OF_MONTH.getFullYear()}-${(
+		FIRST_DAY_OF_MONTH.getMonth() + 1
+	)
+		.toString()
+		.padStart(2, "0")}-${FIRST_DAY_OF_MONTH.getDate()
+		.toString()
+		.padStart(2, "0")}`;
+
+	const GET_TODAY_DATE_FORMAT = `${currentDate.getFullYear()}-${(
+		currentDate.getMonth() + 1
+	)
+		.toString()
+		.padStart(2, "0")}-${currentDate.getDate().toString().padStart(2, "0")}`;
+
+	const [showDateSorting, setShowDateSorting] = useState(false);
+	const [dateFromFilter, setDateFromFilter] = useState(GET_FIRST_DAY_MONTH);
+	const [dateToFilter, setDateToFilter] = useState(GET_TODAY_DATE_FORMAT);
+
+	const resetCalendarDate = () => {
+		setShowDateSorting(false);
+		setDateFromFilter(GET_FIRST_DAY_MONTH);
+		setDateToFilter(GET_TODAY_DATE_FORMAT);
+	};
+
+	const onSubmit = (event: FormEvent) => {
+		event.preventDefault();
+		console.log("dateFromFilter: " + dateFromFilter);
+		console.log("dateToFilter: " + dateToFilter);
+		resetCalendarDate();
+	};
+
+	const onChangeDateFrom = (event: ChangeEvent<HTMLInputElement>) => {
+		setDateFromFilter(event.target.value);
+	};
+
+	const onChangeDateTo = (event: ChangeEvent<HTMLInputElement>) => {
+		setDateToFilter(event.target.value);
+	};
+
 	return (
-		<FlowCashHeader>
-			<FlowCashHeaderTitle>Wydatki na miesiąc</FlowCashHeaderTitle>
-			<FlowCashHeaderOptions>
-				<FlowCashHeaderMonth>09.2023</FlowCashHeaderMonth>
-				<FlowCashHeaderButtons>
-					<CashFlowButton svgUrl="/more_vert.svg" />
-				</FlowCashHeaderButtons>
-			</FlowCashHeaderOptions>
-		</FlowCashHeader>
+		<CashFlowHeader>
+			<CashFlowHeaderTitle>Wydatki na miesiąc</CashFlowHeaderTitle>
+			<CashFlowHeaderOptions>
+				<CashFlowHeaderMonth>09.2023</CashFlowHeaderMonth>
+				<CashFlowHeaderButtons>
+					<CashFlowButton
+						svgUrl="/more_vert.svg"
+						action={() => setShowDateSorting((prevState) => !prevState)}
+					/>
+				</CashFlowHeaderButtons>
+			</CashFlowHeaderOptions>
+
+			<CashFlowSortByDate $animate={showDateSorting}>
+				<CashFlowSortByDateForm onSubmit={onSubmit}>
+					<CashFlowSortByDateLabel htmlFor="sort-by-date">
+						Od:
+					</CashFlowSortByDateLabel>
+					<CashFlowSortByDateInput
+						id="sort-by-date"
+						type="date"
+						max={GET_TODAY_DATE_FORMAT}
+						value={dateFromFilter}
+						onChange={onChangeDateFrom}
+					/>
+					<CashFlowSortByDateLabel htmlFor="sort-by-date">
+						Do:
+					</CashFlowSortByDateLabel>
+					<CashFlowSortByDateInput
+						id="sort-by-date"
+						type="date"
+						max={GET_TODAY_DATE_FORMAT}
+						value={dateToFilter}
+						onChange={onChangeDateTo}
+					/>
+					<CashFlowSortByDataSort>Sortuj</CashFlowSortByDataSort>
+				</CashFlowSortByDateForm>
+			</CashFlowSortByDate>
+		</CashFlowHeader>
 	);
 };
 
