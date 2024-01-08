d5c5dcbd0c5d53009ce18fe96e458a9fd3dce97e
chudymikolaj <chudymikolaj06@gmail.com>
2024-01-08 22:43:19 +0100

    styel: change sm breakpoint

--- styles/breakpoints.tsx
+++ styles/breakpoints.tsx
@@ -1,17 +1,17 @@
 const breakpoints = {
-  xs: "320px",
-  sm: "640px",
-  md: "768px",
-  lg: "992px",
-  xl: "1200px",
-  "2xl": "1440px",
-}
+	xs: "320px",
+	sm: "375px",
+	md: "768px",
+	lg: "992px",
+	xl: "1200px",
+	"2xl": "1440px",
+};
 
 export const devices = {
-  xs: `(min-width: ${breakpoints.xs})`,
-  sm: `(min-width: ${breakpoints.sm})`,
-  md: `(min-width: ${breakpoints.md})`,
-  lg: `(min-width: ${breakpoints.lg})`,
-  xl: `(min-width: ${breakpoints.xl})`,
-  "2xl": `(min-width: ${breakpoints["2xl"]})`,
-}
+	xs: `(min-width: ${breakpoints.xs})`,
+	sm: `(min-width: ${breakpoints.sm})`,
+	md: `(min-width: ${breakpoints.md})`,
+	lg: `(min-width: ${breakpoints.lg})`,
+	xl: `(min-width: ${breakpoints.xl})`,
+	"2xl": `(min-width: ${breakpoints["2xl"]})`,
+};
