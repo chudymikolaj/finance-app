5dbc7f77ddfd618d44e84c140239934a2575d5da
chudymikolaj <chudymikolaj06@gmail.com>
2024-01-08 19:16:24 +0100

    style: change style of CashFlowOnEmpty component

--- components/CashFlow/CashFlowOnEmpty/CashFlowOnEmpty.styles.tsx
+++ components/CashFlow/CashFlowOnEmpty/CashFlowOnEmpty.styles.tsx
@@ -4,6 +4,10 @@ import { FlexCenter } from "@/styles/mixins.styled";
 
 export const CashFlowStyledEmpty = styled.div`
 	${FlexCenter}
+
+	height: 75px;
+	padding: 0 0 15px;
+	border-bottom: 1px solid ${({ theme }) => theme.borderColor};
 `;
 
 export const CashFlowStyledEmptyText = styled.div`
