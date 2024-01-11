d3a04b5118ab3567f4577a32e64a23b91bcc700e
chudymikolaj <chudymikolaj06@gmail.com>
2024-01-11 18:56:10 +0100

    refactore: change BudgetManagement strucutre and remove components

--- components/BudgetManagement/BudgetManagementComponent.styled.tsx
+++ components/BudgetManagement/BudgetManagementComponent.styled.tsx
@@ -1,16 +1,59 @@
-"use client";
-
-import styled from "styled-components";
 import { devices } from "@/styles/breakpoints";
-import { FlexColumn } from "@/styles/mixins.styled";
+import { BORDER_RADIUS, SIZES, WEIGHT } from "@/styles/constants";
+import { FlexColumn, FlexRow } from "@/styles/mixins.styled";
+import styled from "styled-components";
 
-export const BudgetContainer = styled.div`
+export const BudgetManagementAssetsContainer = styled.div`
 	${FlexColumn}
-	gap: 16px;
-
-	@media ${devices.xl} {
-		gap: 30px;
-	}
+	gap: 15px;
 
 	width: 100%;
+	padding: 15px;
+
+	@media ${devices.xl} {
+		padding: 30px;
+	}
+
+	margin: 0 auto 30px;
+	background-color: ${({ theme }) => theme.backgroundElement};
+	border: 1px solid ${({ theme }) => theme.borderColor};
+	border-radius: ${BORDER_RADIUS};
+	position: relative;
+`;
+
+export const BudgetManagementAssetsWrapper = styled.div`
+	@media ${devices.sm} {
+		flex: 1 1 75%;
+	}
+`;
+
+export const BudgetManagementAssetsList = styled.ul`
+	display: grid;
+	grid-template-columns: 1fr;
+	gap: 10px;
+
+	@media ${devices.sm} {
+		grid-template-columns: repeat(2, 1fr);
+		gap: 15px;
+	}
+
+	height: 100%;
+	list-style: none;
+	font-size: ${SIZES.tabAssetsText};
+	font-weight: ${WEIGHT.medium};
+`;
+
+export const BudgetManagementAssetsListItem = styled.li`
+	${FlexColumn}
+	gap: 5px;
+
+	padding: 15px 10px;
+
+	@media ${devices.md} {
+		padding: 15px;
+	}
+
+	background-color: ${({ theme }) => theme.backgroundElement};
+	border: 1px solid ${({ theme }) => theme.borderColor};
+	border-radius: ${BORDER_RADIUS};
 `;
--- components/BudgetManagement/index.tsx
+++ components/BudgetManagement/index.tsx
@@ -1,13 +1,67 @@
-import BudgetAssets from "./BudgetManagementAssets";
-import BudgetCategories from "./BudgetManagementCategories";
+"use client";
 
-const BudgetManagementComponent = () => {
+import React, { useEffect, useState } from "react";
+
+import { useAppContext } from "@/lib/ThemeProviderContext/actions";
+import { type BudgetAllocation } from "@/lib/ThemeProviderContext/ThemeProviderContext.types";
+import BudgetHeader from "./BudgetManagementHeader";
+
+import {
+	BudgetManagementAssetsContainer,
+	BudgetManagementAssetsWrapper,
+	BudgetManagementAssetsList,
+	BudgetManagementAssetsListItem,
+} from "./BudgetManagementComponent.styled";
+import { Line } from "@/components/Line/Line.styles";
+
+const BudgetManagementAssets = () => {
+	const { budgetAllocations, wallet } = useAppContext();
+
+	const [calcedBudget, setCalcedBudget] = useState<BudgetAllocation[]>([]);
+	const isBudgetAllocation = calcedBudget.length !== 0;
+
+	const multiplier = (amount: number) => amount * 100;
+	const mapBudget = (arr: BudgetAllocation[], value: number) => {
+		let valueTest = value;
+
+		const result = arr.map((item) => {
+			return {
+				...item,
+				value: Number(valueTest * item.share),
+			};
+		});
+
+		return result;
+	};
+
+	useEffect(() => {
+		const calcBudget = mapBudget(budgetAllocations, wallet.restRevenues);
+		setCalcedBudget(calcBudget);
+	}, [wallet.restRevenues, budgetAllocations]);
+
 	return (
-		<>
-			<BudgetAssets />
-			<BudgetCategories />
-		</>
+		<BudgetManagementAssetsContainer>
+			<BudgetHeader />
+			<Line />
+			<BudgetManagementAssetsWrapper>
+				<BudgetManagementAssetsList>
+					{isBudgetAllocation ? (
+						calcedBudget.map((asset) => (
+							<BudgetManagementAssetsListItem key={asset.title}>
+								<p>{asset.title}</p>
+								<p>
+									<span>{multiplier(asset.share)}% - </span>
+									<span>{asset.value.toFixed(2)} PLN</span>
+								</p>
+							</BudgetManagementAssetsListItem>
+						))
+					) : (
+						<div>Ładowanie alokacji budżetu</div>
+					)}
+				</BudgetManagementAssetsList>
+			</BudgetManagementAssetsWrapper>
+		</BudgetManagementAssetsContainer>
 	);
 };
 
-export default BudgetManagementComponent;
+export default BudgetManagementAssets;
