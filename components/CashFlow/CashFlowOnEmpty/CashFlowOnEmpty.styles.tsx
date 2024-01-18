c8ee56f30bdae3c35bbb7777849bef72d9d729b6
chudymikolaj <chudymikolaj06@gmail.com>
2024-01-18 21:21:10 +0100

    refactore: add border top and static height CashFlowOnEmpty component

--- components/CashFlow/CashFlowOnEmpty/CashFlowOnEmpty.styles.tsx
+++ components/CashFlow/CashFlowOnEmpty/CashFlowOnEmpty.styles.tsx
@@ -5,8 +5,9 @@ import { FlexCenter } from "@/styles/mixins.styled";
 export const CashFlowStyledEmpty = styled.div`
 	${FlexCenter}
 
-	height: 75px;
+	height: 226px;
 	padding: 0 0 15px;
+	border-top: 1px solid ${({ theme }) => theme.borderColor};
 	border-bottom: 1px solid ${({ theme }) => theme.borderColor};
 `;
 
