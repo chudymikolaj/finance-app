9c63b83b5d2ac0c2eb5ef3dafba1ae97f9230cee
chudymikolaj <chudymikolaj06@gmail.com>
2024-02-16 19:03:41 +0100

    refactor: change Axios getUserData for easier typing endpoint

--- utils/fetch/getUserDataAxios.ts
+++ utils/fetch/getUserDataAxios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function getUserDataAxios<T>(sessions: {}, typeList: string): Promise<T | []> {
 	try {
-		const { data } = await axios.get<T>(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/users/me?populate[${typeList}]=*`, {
+		const { data } = await axios.get<T>(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/users/me${typeList}`, {
 			headers: {
 				Authorization: "Bearer " + (sessions as { jwt: string })?.jwt,
 			},
