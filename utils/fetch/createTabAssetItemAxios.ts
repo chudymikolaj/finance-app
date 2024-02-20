7643a473c7eb39fec826fd8e5bb55491a28205da
chudymikolaj <chudymikolaj06@gmail.com>
2024-02-20 23:24:32 +0100

    feat: add createTabAssetItemAxios to add items to CMS

---
+++ utils/fetch/createTabAssetItemAxios.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+type ItemType = {
+	name: string;
+	value: number;
+	id_asset_item: string;
+	assets_tab: {
+		connect: number[];
+	};
+	users_permissions_user: {
+		connect: number[];
+	};
+};
+
+export default async function createTabAssetItemAxios(item: ItemType, jwt: string, endpoint: string) {
+	try {
+		await axios.post(
+			`${process.env.NEXT_PUBLIC_STRAPI_URL}${endpoint}`,
+			{
+				data: item,
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${jwt}`,
+				},
+			}
+		);
+	} catch (error) {
+		throw error;
+	}
+}
