a9cc3b08f18f16049230aed0ebd5683c3549db25
chudymikolaj <chudymikolaj06@gmail.com>
2024-02-29 21:07:14 +0100

    fix: remove function to changeTabValueAxios

--- utils/fetch/refreshTabAssetsItems.ts
+++ utils/fetch/refreshTabAssetsItems.ts
@@ -1,5 +1,4 @@
 import getUserDataAxios from "./getUserDataAxios";
-import updateTabValueAxios from "./updateTabValueAxios";
 
 import { type AssetTabList } from "@/lib/ThemeProviderContext/ThemeProviderContext.types";
 
@@ -9,22 +8,8 @@ const refreshTabAssetsItems = async (session: any, updateAssetListTabs: (assets_
 		"?populate[assets_tabs][populate][tab_assets]=*"
 	);
 
-	updateAssetListTabs([]);
-
 	if ("assets_tabs" in resultAssetsTabs) {
-		const convertedValueOfTabs = resultAssetsTabs.assets_tabs.map((tab) => {
-			let value = tab.tab_assets.reduce((accumulator, currentValue) => {
-				return accumulator + currentValue.value;
-			}, 0);
-
-			const newValue = { ...tab, value };
-
-			updateTabValueAxios(newValue, (session as any).jwt, "/api/assets-tabs");
-
-			return newValue;
-		});
-
-		updateAssetListTabs(convertedValueOfTabs);
+		updateAssetListTabs(resultAssetsTabs.assets_tabs);
 	}
 };
 
