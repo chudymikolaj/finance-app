e7258692bde9458459f24f80435277ab73d2c4e5
chudymikolaj <chudymikolaj06@gmail.com>
2024-02-29 20:05:34 +0100

    feat: add refreshTabAssetsItems to utils

---
+++ utils/fetch/refreshTabAssetsItems.ts
@@ -0,0 +1,31 @@
+import getUserDataAxios from "./getUserDataAxios";
+import updateTabValueAxios from "./updateTabValueAxios";
+
+import { type AssetTabList } from "@/lib/ThemeProviderContext/ThemeProviderContext.types";
+
+const updateAssetListTabsItem = async (session: any, updateAssetListTabs: (assets_tabs: AssetTabList[]) => void) => {
+	const resultAssetsTabs = await getUserDataAxios(
+		(session as any)?.jwt,
+		"?populate[assets_tabs][populate][tab_assets]=*"
+	);
+
+	updateAssetListTabs([]);
+
+	if ("assets_tabs" in resultAssetsTabs) {
+		const convertedValueOfTabs = resultAssetsTabs.assets_tabs.map((tab) => {
+			let value = tab.tab_assets.reduce((accumulator, currentValue) => {
+				return accumulator + currentValue.value;
+			}, 0);
+
+			const newValue = { ...tab, value };
+
+			updateTabValueAxios(newValue, (session as any).jwt, "/api/assets-tabs");
+
+			return newValue;
+		});
+
+		updateAssetListTabs(convertedValueOfTabs);
+	}
+};
+
+export default updateAssetListTabsItem;
